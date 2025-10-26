6886cd1a486e558614195c395aa87f379b2a3792
Mkots <mkots@yandex.ru>
2025-10-26 20:19:20 +0200

    Enhance Playwright configuration: add Sinon for mocking and implement new tests for advanced functionality

---
+++ src/simple.ts
@@ -0,0 +1,10 @@
+const TYPE_NUMBER = 'number';
+
+export default function add(a: unknown, b: unknown) {
+    // First check if a and b exist (zero is an allowed value), then check their types are correct
+    if ((a || a === 0) && (b || b === 0) && typeof(a) === TYPE_NUMBER && typeof(b) === TYPE_NUMBER) {
+        return Number(a) + Number(b);
+    }
+    // If there's anything wrong with the input data, return false
+    return false;
+}
